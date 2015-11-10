869507a5414c7ec14512d7db8da224b72a567fb8
Thomas Stedronsky <tstedronsky@student.tgm.ac.at>
2015-11-10 14:57:56 +0100

    example ist running

--- start.py
+++ start.py
@@ -1,7 +1,7 @@
-__author__ = 'Thomas'
+"""__author__ = 'Thomas'
 
 import pyglet
-
+import pywavefront
 class solarsystem():
     def __init__(self):
         window = pyglet.window.Window()
@@ -10,15 +10,79 @@ class solarsystem():
                                   font_size=36,
                                   x=window.width//2, y=window.height//2,
                                   anchor_x='center', anchor_y='center')
+        wave = pywavefront.Wavefront('earth.obj')
 
         @window.event
         def on_draw():
             window.clear()
-            label.draw()
+            #label.draw()
+            wave.draw()
 
         @window.event
         def on_key_press(symbol, modifiers):
             print('A key was pressed')
 
         pyglet.app.run()
-solar = solarsystem()
+solar = solarsystem()"""
+
+#!/usr/bin/env python
+"""This script shows another example of using the PyWavefront module."""
+# This example was created by intrepid94
+import sys
+sys.path.append('..')
+import ctypes
+
+import pyglet
+from pyglet.gl import *
+
+from pywavefront import Wavefront
+
+rotation = 0
+
+meshes = Wavefront('earth.obj')
+
+window = pyglet.window.Window(1024, 720, caption = 'Demo', resizable = True)
+
+lightfv = ctypes.c_float * 4
+label = pyglet.text.Label('Hello, world', font_name = 'Times New Roman', font_size = 12, x = 800, y = 700, anchor_x = 'center', anchor_y = 'center')
+@window.event
+def on_resize(width, height):
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    gluPerspective(40.0, float(width)/height, 1, 100.0)
+    glEnable(GL_DEPTH_TEST)
+    glMatrixMode(GL_MODELVIEW)
+    return True
+
+@window.event
+def on_draw():
+    window.clear()
+    glLoadIdentity()
+    glLightfv(GL_LIGHT0, GL_POSITION, lightfv(-40, 200, 100, 0.0))
+    glLightfv(GL_LIGHT0, GL_AMBIENT, lightfv(0.2, 0.2, 0.2, 1.0))
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, lightfv(0.5, 0.5, 0.5, 1.0))
+    glEnable(GL_LIGHT0)
+    glEnable(GL_LIGHTING)
+    glEnable(GL_COLOR_MATERIAL)
+    glEnable(GL_DEPTH_TEST)
+    glShadeModel(GL_SMOOTH)
+    glMatrixMode(GL_MODELVIEW)
+    # glTranslated(0, 4, -8)
+ #    glRotatef(90, 0, 1, 0)
+ #    glRotatef(-60, 0, 0, 1)
+   # Rotations for sphere on axis - useful
+    glTranslated(0, .8, -20)
+    glRotatef(-66.5, 0, 0, 1)
+    glRotatef(rotation, 1, 0, 0)
+    glRotatef(90, 0, 0, 1)
+    glRotatef(0, 0, 1, 0)
+    meshes.draw()
+def update(dt):
+    global rotation
+    rotation += 45 * dt
+    if rotation > 720:
+       rotation = 0
+
+pyglet.clock.schedule(update)
+
+pyglet.app.run()
